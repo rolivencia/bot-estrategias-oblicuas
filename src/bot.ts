d8f48e9cb5830e2acfbe99e489f43275719fda55
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-05-10 05:00:42 -0300

    Agregada funcionalidad para tuitear imagen generada por generateImage()

--- src/bot.ts
+++ src/bot.ts
@@ -1,26 +1,58 @@
 import Twit from 'twit';
-import { getCardText } from './cards';
+import { Card, getCard } from './cards';
+import { generateImage } from './image-generator';
+import { IncomingMessage } from 'http';
 
 const Twitter = new Twit(require('./config'));
 
-const tweetCard = () => {
-  const grab = getCardText();
+const tweetCard = async () => {
+  const grabbedCard: Card = getCard();
+  const bufferedImage:
+    | string
+    | Buffer
+    | (string | Buffer)[] = await generateImage(getCard());
 
   Twitter.post(
-    'statuses/update',
-    { status: grab },
-    function (error, data, response) {
-      if (!error) {
-        console.log(data);
-        console.log(
-          'Success! Check your bot, it should have tweeted something.',
-        );
-      } else {
-        console.log('There was an error with a tweet posting:', error);
-      }
+    'media/upload',
+    {
+      media_data: bufferedImage.toString('base64'),
+    },
+    // FIXME - RO 10/05/21: added "any" to data to be able to grab media_id_string, since the property isn't recognized by Twit.
+    function (
+      err: Error,
+      data: { media_id_string: string } | any,
+      response: IncomingMessage,
+    ) {
+      // now we can assign alt text to the media, for use by screen readers and
+      // other text-based presentations and interpreters
+      const mediaIdStr = data.media_id_string;
+      const altText = grabbedCard.quote;
+      const meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
+
+      Twitter.post(
+        'media/metadata/create',
+        meta_params,
+        function (err, data, response) {
+          if (!err) {
+            // now we can reference the media and post a tweet (media will attach to the tweet)
+            const params = {
+              status: '',
+              media_ids: [mediaIdStr],
+            };
+
+            Twitter.post(
+              'statuses/update',
+              params,
+              function (err, data, response) {
+                console.log(data);
+              },
+            );
+          }
+        },
+      );
     },
   );
 };
 
 tweetCard();
-setInterval(tweetCard, 1000 * 60 * 60 * 6);
+setInterval(tweetCard, 1000 * 60 * 60 * 3);
