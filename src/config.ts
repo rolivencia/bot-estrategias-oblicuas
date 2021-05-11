e8718ddb652230c9791a71e0a0ca345613d2056f
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-05-10 23:36:50 -0300

    Corrección en definición de environment

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
 import * as dotenv from 'dotenv';
 dotenv.config({ path: `.env` });
 
-module.exports = {
+const environment = {
   consumer_key: process.env.consumer_key,
   consumer_secret: process.env.consumer_secret,
   access_token: process.env.access_token,
   access_token_secret: process.env.access_token_secret,
 };
+
+module.exports = environment;
