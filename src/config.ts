a7abaa60a1a19af0188242bbe33af8fa58b95aeb
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-05-10 21:34:17 -0300

    Implementada librería dotenv para manejo de variables de entorno

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: `.env` });
+
 module.exports = {
-  consumer_key: 'HXI1P3nJDuFnPE4qrRIMryzTj',
-  consumer_secret: 'EMUKhoZwKwhawxd84AT5fGQg4Ezj1gOMULk9DceWsuvh6oKyUU',
-  access_token: '1363383751393366016-shAa4HuruKSolqYYiZ9yhVGsA5MLN4',
-  access_token_secret: 'msEK5okQCo8Mdf25JRvnSsqF0BWAC3y6G40L8eGAPbI1s',
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
 };
