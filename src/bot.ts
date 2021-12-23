81263aabfe874832ca6bb62c646979dc123af040
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-12-23 01:40:55 -0300

    Replacement of twit for new twitter-api-v2. Added new implementation of tweetCard method for regular post use case

--- src/bot.ts
+++ src/bot.ts
@@ -1,72 +1,83 @@
-import Twit from 'twit';
-import { Card, getCard } from './cards';
+import { getCard } from './cards';
 import { generateImage } from './image-generator';
-import { IncomingMessage } from 'http';
+import TwitterApi from 'twitter-api-v2';
 import 'regenerator-runtime/runtime';
 
-const Twitter = new Twit(require('./config'));
+const twitterClient = new TwitterApi(require('./config'));
 
-const tweetCard = async (tweet?: Twit.Twitter.Status) => {
-  const grabbedCard: Card = getCard();
+const tweetCard = async (tweet?: any) => {
   const bufferedImage: string | Buffer | (string | Buffer)[] =
     await generateImage(getCard());
-
-  const params: {
-    media_data?: string;
-    status?: string;
-    in_reply_to_status_id?: string;
-  } = { media_data: bufferedImage.toString('base64') };
-
-  Twitter.post(
-    'media/upload',
-    params,
-    // FIXME - RO 10/05/21: added "any" to data to be able to grab media_id_string, since the property isn't recognized by Twit.
-    function (
-      err: Error,
-      data: { media_id_string: string } | any,
-      response: IncomingMessage,
-    ) {
-      // now we can assign alt text to the media, for use by screen readers and
-      // other text-based presentations and interpreters
-      const mediaIdStr: string = data.media_id_string;
-      const altText = grabbedCard.quote;
-      const meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
-
-      Twitter.post('media/metadata/create', meta_params, function (err) {
-        if (!err) {
-          // now we can reference the media and post a tweet (media will attach to the tweet)
-          const params: {
-            media_ids: string[];
-            status?: string;
-            in_reply_to_status_id?: string;
-          } = {
-            status: '',
-            media_ids: [mediaIdStr],
-          };
-
-          if (tweet) {
-            // User handle of who sent the mention
-            const name = tweet.user.screen_name;
-            const nameId = tweet.id_str;
-
-            // Reply to sender
-            const reply = 'Esta es tu estrategia oblicua, @' + name;
-            params.status = reply;
-            params.in_reply_to_status_id = nameId;
-          }
-
-          Twitter.post('statuses/update', params, (err, data) =>
-            console.log(data),
-          );
-        }
-      });
-    },
-  );
+  const mediaIds = await Promise.all([
+    twitterClient.v1.uploadMedia(bufferedImage as Buffer, { type: 'png' }),
+  ]);
+  await twitterClient.v1.tweet('', {
+    media_ids: mediaIds,
+  });
 };
 
-const stream = Twitter.stream('statuses/filter', { track: '@EOblicuasBot' });
-
-stream.on('tweet', tweetCard);
-
 void tweetCard();
-setInterval(tweetCard, 1000 * 60 * 60 * 6);
+setInterval(tweetCard, 1000 * 60 * 60 * 24);
+
+// const tweetCard2 = async (tweet?: Twit.Twitter.Status) => {
+//   const grabbedCard: Card = getCard();
+//   const bufferedImage: string | Buffer | (string | Buffer)[] =
+//     await generateImage(getCard());
+//
+//   const params: {
+//     media_data?: string;
+//     status?: string;
+//     in_reply_to_status_id?: string;
+//   } = { media_data: bufferedImage.toString('base64') };
+//
+//   Twitter.post(
+//     'media/upload',
+//     params,
+//     // FIXME - RO 10/05/21: added "any" to data to be able to grab media_id_string, since the property isn't recognized by Twit.
+//     function (
+//       err: Error,
+//       data: { media_id_string: string } | any,
+//       response: IncomingMessage,
+//     ) {
+//       // now we can assign alt text to the media, for use by screen readers and
+//       // other text-based presentations and interpreters
+//       const mediaIdStr: string = data.media_id_string;
+//       const altText = grabbedCard.quote;
+//       const meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
+//
+//       Twitter.post('media/metadata/create', meta_params, function (err) {
+//         if (!err) {
+//           // now we can reference the media and post a tweet (media will attach to the tweet)
+//           const params: {
+//             media_ids: string[];
+//             status?: string;
+//             in_reply_to_status_id?: string;
+//           } = {
+//             status: '',
+//             media_ids: [mediaIdStr],
+//           };
+//
+//           if (tweet) {
+//             // User handle of who sent the mention
+//             const name = tweet.user.screen_name;
+//             const nameId = tweet.id_str;
+//
+//             // Reply to sender
+//             const reply = 'Esta es tu estrategia oblicua, @' + name;
+//             params.status = reply;
+//             params.in_reply_to_status_id = nameId;
+//           }
+//
+//           Twitter.post('statuses/update', params, (err, data) =>
+//             console.log(data),
+//           );
+//         }
+//       });
+//     },
+//   );
+// };
+
+// const stream = Twitter.stream('statuses/filter', { track: '@EOblicuasBot' });
+
+// stream.on('tweet', tweetCard);
+
--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,10 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: `.env` });
 
 const environment = {
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token: process.env.access_token,
-  access_token_secret: process.env.access_token_secret,
+  appKey: process.env.consumer_key,
+  appSecret: process.env.consumer_secret,
+  accessToken: process.env.access_token,
+  accessSecret: process.env.access_token_secret,
 };
 
 module.exports = environment;
